85ad03443da257dfb0fc2d2b881e747d170ffa8e
=Hugo <=A23HugodC@iessanclemente.net>
2023-11-21 13:46:18 +0100

    primera version funcional

--- GestorDeTareas.java
+++ GestorDeTareas.java
@@ -1,7 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * GestorDeTareas
  */
 public class GestorDeTareas {
+    public static void main(String[] args) {
+        Scanner escaner = new Scanner(System.in);
 
-    /*CREAR CLASE GESTOR DE TAREAS */
+        ArrayList<Tarea> arrayTareas = new ArrayList<Tarea>();
+
+        while (true) {
+            System.out.println("a) Crear tarea\nb) Borrar tarea\nc) Mostrar tareas");
+            String opcion = escaner.nextLine();
+
+            switch (opcion) {
+                case "a":
+                    System.out.println("Nombre");
+                    String nombre = escaner.nextLine();
+                    System.out.println("Descripcion");
+                    String descripcion = escaner.nextLine();
+                    System.out.println("Estado");
+                    String estado = escaner.nextLine();
+                    Tarea tareaNew = new Tarea(nombre, descripcion, estado);
+                    arrayTareas.add(tareaNew);
+                    break;
+                case "b":
+                    System.out.println("ID tarea a borrar");
+                    int id = escaner.nextInt();
+                    for (Tarea i : arrayTareas) {
+                        if (i.getId_tarea() == id) {
+                            arrayTareas.remove(i);
+                        }
+                    }
+                    break;
+                case "c":
+                    for (Tarea i : arrayTareas) {
+                        System.out.println("ID: "+i.getId_tarea()+" , Nombre: "+i.getNombre()+" , Estado: "+i.getEstado()+" , Descripcion: "+i.getDescripcion());
+                    }
+                    break;
+            }
+        }
+    }
 }
--- Tarea.java
+++ Tarea.java
@@ -3,5 +3,45 @@
  */
 public class Tarea {
 
-    /*CREAR CLASE TAREA */
+    public static int id = 0;
+
+    private int id_tarea;
+    private String nombre;
+    private String descripcion;
+    private String estado;
+
+    public Tarea(String nombre, String descripcion, String estado) {
+        this.id_tarea = id++;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public int getId_tarea() {
+        return id_tarea;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
